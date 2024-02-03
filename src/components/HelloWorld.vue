fe3d7f6f9441d13e77473a25023110ca87057cab
DonatoMancera <donatelomancera@hotmail.com>
2024-02-02 20:26:10 -0500

    actualizacion1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 class="title">{{ msg }}</h1>
+    <div class="container">
+      <div v-for="(card, index) in cards" :key="index" class="row mb-4">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="row no-gutters">
+              <div class="col-md-1 d-flex justify-content-center align-items-center">
+                <img :src="card.image" alt="Imagen" class="card-img" style="width: 70px; height: 70px; object-fit: cover;">
+              </div>
+              <div class="card-body col-md-11">
+                  <div class="mb-2 d-flex">
+                    <a class="mb-0 pb-0 text-left bold" href="#" style="text-decoration: none; color: black ;">
+                      <h5 class="d-md-inline-flex">{{card.title}}</h5>
+                    </a>
+                  </div>
+                  <div class="row">
+                    <div class="col-2 d-flex">
+                      <p>{{card.company}}</p>
+                    </div>
+                    <div class="col-2">
+                      <p>{{card.country}}</p>
+                    </div>
+                    <div class="col-2 ">
+                      <p>{{card.salary}}</p>
+                    </div>
+                    <div class="col-2 ">
+                      <p>{{card.vacancies}}</p>
+                    </div>
+                    <div class="col-2 ">
+                      <p>{{card.date}}</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,24 +46,52 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data(){
+    return {
+      cards: [
+      { 
+        title: 'Ingeniero de Calidad', 
+        company:"OFFIMEDICAS SAS",
+        country: "Bogota",
+        salary: "$ 1.4M - 1.6M COP",
+        vacancies: "1 vacante",
+        date: 'hace 2 horas', 
+        image: 'https://images.pexels.com/photos/681467/pexels-photo-681467.jpeg' 
+      },
+      { 
+        title: 'IT Sales Consultant', 
+        company: "INTEXUS",
+        country: "Medellin",
+        salary: "$ 1.8M - 2M COP",
+        vacancies: "1 vacante",
+        date: 'hace dos minutos', 
+        image: 'https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+      },
+      { 
+        title: 'Senior Full Stack Developer', 
+        company: "20 MOVES",
+        country: "Barranquilla",
+        salary: "$ 8M - 10M COP",
+        vacancies: "1 vacante",
+        date: 'hace dos meses', 
+        image: 'https://images.pexels.com/photos/2101187/pexels-photo-2101187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    ]
+    }
+  },  
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+  .card {
+  transition: box-shadow 0.3s;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+
+.card:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
 }
 </style>
